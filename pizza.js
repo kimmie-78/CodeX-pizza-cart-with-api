db99d6d6c4dc4d3c521c4c964daa5f46934c8064
unknown <kimberelyisaac217@gmail.com>
2024-07-19 11:12:55 +0200

    edit commit

--- pizza.js
+++ pizza.js
@@ -3,6 +3,43 @@ document.addEventListener("alpine:init", () => {
         return {
             title: 'Pizza Cart API',
             pizzas: [],
+            username: 'kimmie-78',
+            cartId: 'kv4xr0kv44',
+            cartPizzas: [],
+            cartTotal: 0.00,
+            getCart() {
+                const getCartUrl = `https://pizza-api.projectcodex.net/api/pizza-cart/${this.cartId}/get`
+                return axios.get(getCartUrl)
+            },
+            addPizza(pizzaId) {
+
+                return axios.post('https://pizza-api.projectcodex.net/api/pizza-cart/add',{
+                    "cart_code" : this.cartId,
+                    "pizza_id" :pizzaId
+             } )
+
+
+            },
+            removePizza(pizzaId) {
+
+                return axios.post(' https://pizza-api.projectcodex.net/api/pizza-cart/remove',{
+                    "cart_code" : this.cartId,
+                    "pizza_id" :pizzaId
+             } )
+
+
+            },
+
+            showCartData() {
+                this.getCart().then(result => {
+                    console.log(result.data);
+                    const cartData = result.data;
+                    this.cartPizzas = cartData.pizzas;
+                    this.cartTotal = cartData.total.toFixed(2);
+                    //alert(this,this.cartTotal);
+                })
+
+            },
             init() {
                 axios
                     .get('https://pizza-api.projectcodex.net/api/pizzas')
@@ -10,10 +47,34 @@ document.addEventListener("alpine:init", () => {
 
                         //console.log(result.data);
                         this.pizzas = result.data.pizzas
-                    })
+                    });
+
+                this.showCartData();
+
+            },
+            addPizzaToCart(pizzaId) {
+                //alert(pizzaId)
+                this.addPizza(pizzaId)
+                .then(() =>{
+                    this.showCartData();
+                    
+                }
+                )
+
+
+            },
+            removePizzaFromCart(pizzaId) {
+                //alert(pizzaId)
+                this.removePizza(pizzaId)
+                .then(() =>{
+                    this.showCartData();
+                    
+                }
+                )
+
             }
         }
-        
+
     });
 
 
